0b12907b34ff95126245ef766cc5ca0c0a45d244
Kopantsho Mathafa <849038@students.wits.ac.za>
2019-05-10 20:27:11 +0200

    Included .gitignore and finalized write

--- write.c
+++ write.c
@@ -1,19 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <mpi.h>
-int main(intargc, char **argv)
-{int i, rank, size, offset, nints, N=16;
-MPI_File fhw;
-MPI_Status status;
-MPI_Init(&argc, &argv);
-MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-MPI_Comm_size(MPI_COMM_WORLD, &size);
-int buf[N];
-for ( i=0;i<N;i++){buf[i] = i;}
+int main(int argc, char **argv)
+{
+    int i, rank, size, offset, nints, N = 16;
+    MPI_File fhw;
+    MPI_Status status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int buf[N];
 
-offset = rank*(N/size)*sizeof(int);
-MPI_File_open(MPI_COMM_WORLD, "datafile", MPI_MODE_CREATE|MPI_MODE_WRONGLY, MPI_INFO_NULL, &fhw);
-print("\nRank: %d, Offset: %d\n", rank, offset);
-MPI_File_write_at(fhw, offset, buff, (N/size), MPI_INT, &status);
-MPI_Finalize();
-return 0;
+    for (i = 0; i < N; i++)
+    {
+        buf[i] = i;
+    }
+
+    offset = rank * (N / size) * sizeof(int);
+    MPI_File_open(MPI_COMM_WORLD, "datafile.out", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fhw);
+    printf("\nRank: %d, Offset: %d\n", rank, offset);
+    MPI_File_write_at(fhw, offset, buf, (N / size), MPI_INT, &status);
+    MPI_Finalize();
+    return 0;
 }
