6781ffc2e8695cd3c43f3aa9ce18b7c488ec96df
Unknown <1036603@students.wits.ac.za>
2019-05-12 14:30:52 +0200

    Added File Basics

---
+++ blockingKeyValue.h
@@ -0,0 +1,26 @@
+#ifndef BLOCKINGKEYVALUE_H
+#define BLOCKINGKEYVALUE_H
+#include <vector>
+#include <iostream>
+
+typedef std::vector<std::vector<int>> r2Vec;
+
+class blockingKeyValue
+{
+public:
+	blockingKeyValue()
+	{
+	}
+	~blockingKeyValue()
+	{
+	}
+	 void blockMatrix(const auto* inMatrix, const int blockSize)
+	 {//Creates a map of sub matrices. The key will be the position of the sub matrix relative to the large Matrix. The value will be the sub matrix
+		 
+	 }
+private:
+
+
+};
+
+#endif // BLOCKINGKEYVALUE_H
