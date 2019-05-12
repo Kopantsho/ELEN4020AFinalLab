d2e35d5f2df4d450d86ee4ab0c825b32ebdb107b
Unknown <1036603@students.wits.ac.za>
2019-05-12 08:47:58 +0200

    Added additional Functionality

--- fileMatters.h
+++ fileMatters.h
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <vector>
 static int matrixSize;
+typedef std::vector<std::vector<int>> r2Vec;
 class fileMatters
 {
 public:
@@ -21,9 +22,8 @@ public:
 	{//Generate file of row column order matrix in binary file
 		std::vector<int> numbers;
 		//Generate Random Numbers
-		for(auto i = 0; i< matrixSize; i++) {
+		for(auto i = 0; i< matrixSize*matrixSize; i++) {
 			numbers.push_back(std::rand() %100);
-			std::cout<<numbers.at(i)<<std::endl;
 		}
 		
 		std::ofstream outStream("matrix", std::ios::binary);
@@ -35,24 +35,47 @@ public:
 			outStream.close();
 		}
 		
-		
-	
-	
 	void readFromMatrixFile()
-	{
+	{//Read the matrix values from the generated file
 		int test;
 		std::ifstream inStream("matrix", std::ios::binary);
 		if(inStream.is_open())
 		{
-			while(!inStream.eof())
+			while(inStream.peek() != EOF)
 			{
-			inStream.read(reinterpret_cast<char*>(&test), sizeof(int));	
+			for(auto i = 0; i < matrixSize; i++){
+				std::vector<int> tempVec;
+				for(auto j = 0; j<matrixSize; j++){
+					inStream.read(reinterpret_cast<char*>(&test), sizeof(int));	
+					tempVec.push_back(test);
+				}
+				matrix.push_back(tempVec);
+			}
+			
 			}
 			
 			inStream.close();
 		}
+		
+	}
+	
+	void printCurMatrixInMem()
+	{//Prints the current stored matrix
+		for(auto i = 0; i < matrixSize; i++){
+			
+			for(auto j = 0; j<matrixSize; j++){
+				std::cout<<matrix.at(i).at(j)<<' ';
+			}
+			std::cout<<'\n';
+			}
+	}
+	
+	r2Vec getMatrix()
+	{//Returns the current matrix
+		return matrix;
 	}
 private:
+	r2Vec matrix;
 };
 
 #endif // FILEREADER_H
