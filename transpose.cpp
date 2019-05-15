8b3363177bbf6c86f0824b16bd6ecb6d69a9a69d
James <1036603@students.wits.ac.za>
2019-05-16 00:12:12 +0200

    Changing to serial. Use this file

--- transpose.cpp
+++ transpose.cpp
@@ -5,8 +5,14 @@
 #include <iostream>
 #include "fileMatters.h"
 #include "blockingKeyValue.h"
+#define MATRIX_SIZE 9
 using namespace std;
-typedef std::vector<std::vector<int>> r2Vec;
+struct matrixElement{
+  int x;
+  int y;
+  int val;
+};
+
 
 void SerialMatrixTranspose(auto* inMatrix)
 //Transposes a NxN matrix without any threading
@@ -28,7 +34,7 @@ void SerialMatrixTranspose(auto* inMatrix)
         inMatrix->at(k).subBlock = tempVec;
     }
     /*
-   
+
      * */
 }
 
@@ -37,22 +43,31 @@ void SerialMatrixTranspose(auto* inMatrix)
 using namespace std;
 int main(int argc, char **argv)
 {
-	
-	
+
+
 	srand(time(0));
-	fileMatters myFile(9);
-	myFile.generateRandomMatrixFile();
-	myFile.readFromMatrixFile();	
-	r2Vec matrix = myFile.getMatrix();
-	myFile.printMatrix(matrix);
-	
-	blockingKeyValue blocker;
-	auto mapping =  blocker.blockMatrix(matrix, 3);
-	blocker.printMap(mapping);
-    SerialMatrixTranspose(&mapping);
-    blocker.printMap(mapping);
-    
-    
+	fileMatters myFile(MATRIX_SIZE);
+  auto arr= myFile.getElements();
+  for(auto i = 0; i< MATRIX_SIZE*MATRIX_SIZE; i++){
+    cout<<arr[i]<<endl;
+  }
+  //Serial the elements into coordinate pairs with value. Function was giving segmentation fault
+  matrixElement elements[MATRIX_SIZE*MATRIX_SIZE];
+  auto counter = 0;
+  for(auto i = 0; i < MATRIX_SIZE; i++){
+    for(auto j = 0; j < MATRIX_SIZE; j++){
+      elements[counter].x = j;
+      elements[counter].y = i;
+      elements[counter].val = arr[counter];
+      counter++;
+    }
+  }
+
+  for(auto i = 0; i< MATRIX_SIZE*MATRIX_SIZE; i++){
+    cout<<elements[i].y<<' '<<elements[i].x<<' '<<elements[i].val<<endl;
+  }
+
+
 
 	return 0;
 }
